d823ca8b7b453cfc1fb94530ecdbc0885caaba24
hammamiomar <oh524@nyu.edu>
2025-09-17 21:44:18 -0400

    added env vars

---
+++ setup_env.sh
@@ -0,0 +1,7 @@
+  #!/bin/bash
+  export UV_LINK_MODE=copy
+  export UV_SYSTEM_PYTHON=1
+  export UV_COMPILE_BYTECODE=1
+  export HF_HOME=/workspace/.cache/huggingface
+  export TRANSFORMERS_CACHE=/workspace/.cache/huggingface/hub
+  export HF_DATASETS_CACHE=/workspace/.cache/huggingface/datasets
